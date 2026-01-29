d10f1e35da84ec569d72d93cabfdd5427fc2944b
chanakya <you@example.com>
2026-01-29 13:16:30 +0530

    actions

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-P1= CHANAKYA
-P2= CHANTI
+P1=CHANAKYA    # no space bettwen value and =
+P2=CHANTI
 
 echo "$P1:: Hello $P2,How are you?"
 echo "$P2:: Hi $P1,am fine. What about you?"
