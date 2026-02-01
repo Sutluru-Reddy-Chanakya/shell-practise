271cff56e8c4921471f171eba142a47b7152ea0b
chanakya <you@example.com>
2026-02-01 10:25:47 +0530

    actions

--- 10-Install.sh
+++ 10-Install.sh
@@ -4,10 +4,33 @@ USERID=$(id -u)
 
 echo "$USERID is equal to 0"
 
-if [$USERID -eq 0] ; then
+if [$USERID -ne 0] ; then
     echo "please run this script with root assess"
+    exit 1
 
 fi
 
+dnf install my sql -y
+
 echo "installing Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0]; then
+     echo "failure nginx"
+     exit 1
+
+else 
+   echo "sucess nginx"
+fi
+
+dnf nodejs -y
+
+if [ $? -ne 0]; then
+     echo "failure nodejs"
+     exit 1
+
+else 
+   echo "sucess nodejs"
+fi
+
+   
