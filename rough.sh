14299625444abc92e5db2c975655edcfc21a69e5
chanakya <you@example.com>
2026-02-02 17:28:18 +0530

    origin

--- rough.sh
+++ rough.sh
@@ -5,7 +5,6 @@ USERID=$(id -u)
 echo "$USERID is equal to 0"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run this script with root assess"
+    echo "please run this script with root access"
     exit 1
-
 fi
