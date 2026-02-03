5cfdff8a069e6dc182e24155232bdb5ce8fdb6e3
chanakya <you@example.com>
2026-02-03 16:01:28 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 LOGS_FOLDER= "/var/log/shell-script"
 LOGS_FILE= "/var/log/shell-script/$0.logs"
+
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
     exit 1
@@ -12,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2 ... FAILURE"
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
