cdd0437a91a2a7fee8d6f8ac05ece0ebb187074a
chanakya <you@example.com>
2026-02-03 16:02:35 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
-LOGS_FOLDER= "/var/log/shell-script"
-LOGS_FILE= "/var/log/shell-script/$0.logs"
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access"
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
