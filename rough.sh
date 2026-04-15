195e3e615a98edc5c649a427bf64d2122b955dc0
chanakya <you@example.com>
2026-04-15 16:14:23 +0530

    shell

--- rough.sh
+++ rough.sh
@@ -5,19 +5,6 @@ USERID=$(id -u)
 echo "$USERID is equal to 0"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run this script with root assess"
+    echo "please run this script with root access"
     exit 1
-    
-
 fi
-
-echo "installing nginx"
-
-dnf install nginx -y
-
-
-# OUTPUT:
-# 1001 is equal to 0
-# please run this script with root assess
-# installing nginx
-# Error: This command has to be run with superuser privileges (under the root user on most systems).
