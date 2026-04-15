52524c48069f5885e52c9e2718e5b11afd24ec4b
chanakya <you@example.com>
2026-04-15 12:41:00 +0530

    shell

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -20,16 +20,27 @@ fi
 
 
 
-# Number=$1
-
-# if [ $Number -gt 20 ]; then
-#     echo "Given number: $Number IS GREATER than 20".
-
-# elif [ $Number -eq 20 ]; then
-# echo "Given number: $Number IS equal than to  20"
 
 
-# else
-# echo "Given number: $Number IS Lessthan than 20"
+Number=$1
 
-# fi
+if [ $Number -gt 20 ]; then
+    echo "Given number: $Number IS GREATER than 20".
+
+elif [ $Number -eq 20 ]; then
+echo "Given number: $Number IS equal than to  20"
+
+
+else
+echo "Given number: $Number IS Lessthan than 20"
+
+fi
+
+
+
+# OUTPUT:
+# sh 09-conditions.sh 10
+# Given number: 10 IS Lessthan than 20
+
+# [ root@ip-172-31-28-251 ~/shell-practise ]# sh 09-conditions.sh 21
+# Given number: 21 IS GREATER than 20.
