e5784758c0e2a904146038c7272daa3ac851ce00
chanakya <you@example.com>
2026-04-15 19:17:54 +0530

    shell

---
+++ 15-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+
+for package in $@
+
+do
+  dbf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed. Installing $package..." | tee -a $LOGS_FILE
+    else
+        echo "$package is already installed. Skipping installation." | tee -a $LOGS_FILE
+        continue
+    fi
+  dnf install $package -y &>>$LOGS_FILE
+  VALIDATE $? "$package installlation"  
+done
