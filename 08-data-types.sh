481489b3c3196c410c7849e3deb4babb5772011d
chanakya <you@example.com>
2026-04-15 12:35:44 +0530

    shell

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -21,6 +21,18 @@ echo "Frurits are: ${FRUITS[1]}"
 echo "Frurits are: ${FRUITS[2]}"
 
 
+# OUTPUT:
+
+# [ root@ip-172-31-28-251 ~/shell-practise ]# sh 08-data-types.sh
+# sum is 100
+# Frurits are: Apples Bananna pomo
+# Frurits are: Apples, Bananna, pomo
+# Frurits are: Apples,
+# Frurits are: Bananna,
+# Frurits are: pomo
+
+#NOTES:
+
 # data types
 # ==============
 # variables are holding data..
--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-Number=$1
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal
+# -ne -> not equal
+
+Number=20
 
 if [ $Number -gt 20 ]; then
     echo "Given number: $Number IS GREATER than 20".
 
-elif [ $Number -eq 20 ]; then
-echo "Given number: $Number IS equal than to  20"
 
 
 else
 echo "Given number: $Number IS Lessthan than 20"
 
 fi
+
+
+
+
+# Number=$1
+
+# if [ $Number -gt 20 ]; then
+#     echo "Given number: $Number IS GREATER than 20".
+
+# elif [ $Number -eq 20 ]; then
+# echo "Given number: $Number IS equal than to  20"
+
+
+# else
+# echo "Given number: $Number IS Lessthan than 20"
+
+# fi
